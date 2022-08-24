6d14533fe54b28579d8c4d273bd8f0a44a883751
Administrator <littlelumos@foxmail.com>
2022-08-25 07:07:44 +0800

    add:尝试const

--- main.js
+++ main.js
@@ -3,11 +3,8 @@
 // js字符串方法 https://www.runoob.com/js/js-strings.html
 // git提交规范 https://www.jianshu.com/p/752edeae4855
 function test(){
-    var x = 10;
+    const x = 10;
     alert(x);
-    {
-        let x = 2;
-        alert(x);
-    }
+    x = x+10;
     alert(x);
 }
