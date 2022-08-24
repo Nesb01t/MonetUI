2dc7037340d47a166bdcf506aaf77ce86fce8e77
Administrator <littlelumos@foxmail.com>
2022-08-25 07:06:37 +0800

    add:块级let

--- main.js
+++ main.js
@@ -3,5 +3,11 @@
 // js字符串方法 https://www.runoob.com/js/js-strings.html
 // git提交规范 https://www.jianshu.com/p/752edeae4855
 function test(){
-    alert(this);
+    var x = 10;
+    alert(x);
+    {
+        let x = 2;
+        alert(x);
+    }
+    alert(x);
 }
