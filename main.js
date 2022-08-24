04fd58e0f20f530edad605d84ecde8ac946f55b1
Administrator <littlelumos@foxmail.com>
2022-08-25 07:12:51 +0800

    add:调用json

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 // js字符串方法 https://www.runoob.com/js/js-strings.html
 // git提交规范 https://www.jianshu.com/p/752edeae4855
 function test(){
-    const x = 10;
-    alert(x);
-    x = x+10;
-    alert(x);
+    alert(test_json.site[1].name);
 }
+
+var test_json = {"site":[
+    {"name":"player", "url":"www.baidu.com"},
+    {"name":"player2", "url":"www.baidu.com"},
+]}
