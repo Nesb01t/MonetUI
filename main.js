404fa10a411d1fc5b070bca2fdf7053cccd9692a
Administrator <littlelumos@foxmail.com>
2022-08-25 07:51:33 +0800

    add:js完结

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // 
 // js字符串方法 https://www.runoob.com/js/js-strings.html
 // git提交规范 https://www.jianshu.com/p/752edeae4855
+// js类(class) https://www.runoob.com/js/js-class-intro.html
 function test(){
     document.getElementById("txt").innerHTML=Date();
     setTimeout(test, 1000)
