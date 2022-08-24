24db46453f74c61a0dd6dcc6884131a18f63f30e
Administrator <littlelumos@foxmail.com>
2022-08-25 07:23:48 +0800

    add:js里的void(0)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // js字符串方法 https://www.runoob.com/js/js-strings.html
 // git提交规范 https://www.jianshu.com/p/752edeae4855
 function test(){
-    alert(test_json.site[1].name);
+    alert(javascript:void(0));
 }
 
 var test_json = {"site":[
