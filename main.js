ed231134532e029d012a805a633beb6ed5fed6e8
Administrator <littlelumos@foxmail.com>
2022-08-25 06:42:35 +0800

    js解决html事件

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-function myOut(i){ // 调试输出
-    if (i==0) {
-        window.alert("alert!");
-    } else if (i==1) {
-        document.write("doc.write!");
-    } else if (i==2) {
-        console.log("log out!");
-    }
-}
-
-var personOut = {
-    firstname:"John",
-    age:50,
-
-    out:function(){
-        alert("yes");
-        return this.firstname;
-    }
+function eventhandler(){
+    alert("yes!");
 }
