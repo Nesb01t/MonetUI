04dbc613bcef2d73e8992a559dd4dc9861a20fdf
Administrator <littlelumos@foxmail.com>
2022-08-25 07:29:02 +0800

    add:相关setTimeout

--- main.js
+++ main.js
@@ -3,8 +3,9 @@
 // js字符串方法 https://www.runoob.com/js/js-strings.html
 // git提交规范 https://www.jianshu.com/p/752edeae4855
 function test(){
-    alert(javascript:void(0));
+    alert("1");
 }
+setTimeout(test, 1200)
 
 var test_json = {"site":[
     {"name":"player", "url":"www.baidu.com"},
