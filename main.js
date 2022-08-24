cae36b1a0676b5342a5ee4dd95ada99d2d75875c
Administrator <littlelumos@foxmail.com>
2022-08-25 06:47:14 +0800

    add:try代码

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // link of code learning
 // 
 // js字符串方法 https://www.runoob.com/js/js-strings.html
+// git提交规范 https://www.jianshu.com/p/752edeae4855
 function eventhandler(){
-    alert("yes!");
+   try {
+    alerts("qwq");
+   } catch(err) {
+    alert(err.message)
+   }
 }
