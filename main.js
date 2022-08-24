8a6081d0c2537ea7ecc1249da4d5fc2526928223
Administrator <littlelumos@foxmail.com>
2022-08-25 07:03:25 +0800

    add:函数调用this全局对象

--- main.js
+++ main.js
@@ -3,6 +3,5 @@
 // js字符串方法 https://www.runoob.com/js/js-strings.html
 // git提交规范 https://www.jianshu.com/p/752edeae4855
 function test(){
-    var x = document.forms["myForm"]["inputName"].value;
-    alert("你输入了 -> "+x)
+    alert(this);
 }
