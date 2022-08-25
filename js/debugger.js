2e86f787ed99a27125f5991bee8cba547f50549d
Administrator <littlelumos@foxmail.com>
2022-08-25 23:44:38 +0800

    add:Boolean类型转换

--- js/debugger.js
+++ js/debugger.js
@@ -10,8 +10,15 @@
 // alert(typeof undef+"\n当坚持做到变量声明时都初始化， 则typeof undef=未声明");
 
 // -> null类型
-var x = null;
-alert(typeof x+"\nnull用于obj类型初始化");
-if (x != null){
-    alert("x对象已有内容!");
+// var x = null;
+// alert(typeof x+"\nnull用于obj类型初始化");
+// if (x != null){
+//     alert("x对象已有内容!");
+// }
+
+// -> 转换成Boolean时发生的事情(可以类推)
+if("abc"){
+    alert("有内容");
+} else {
+    alert("NOTHING HERE");
 }
