d0a32774444569084ba763aba9859746aaa75e87
Administrator <littlelumos@foxmail.com>
2022-08-29 07:42:20 +0800

    update:js参数数组

--- js/debugger.js
+++ js/debugger.js
@@ -1,17 +1,15 @@
 // debugger header
-"use strict"; //使用严格模式
+// "use strict"; //使用严格模式
 function out(i){
     document.getElementById("txt").innerHTML=
     document.getElementById("txt").innerHTML+", "+i;
     return;
 }
 
-// -> switch
-switch (1) {
-    case 2:
-        out(2);
-        break;
-    default:
-        out(1);
-        break;
+// -> js参数数组
+function arg_out(i){
+    for (const x in arguments) {
+        out(arguments[x]);
+    }
 }
+arg_out(1, "qwq", 3)
