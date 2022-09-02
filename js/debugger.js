ccbb0466fac2ee41e0b75cb8789103549a8c3e5e
Administrator <littlelumos@foxmail.com>
2022-09-02 17:51:51 +0800

    update:递归函数中耦合情况

--- js/debugger.js
+++ js/debugger.js
@@ -6,8 +6,12 @@ function out(i){
     return;
 }
 
-// -> 函数声明和表达式初始化(2)
-out(a(1));
-var a = function(x){
-    return x+1;
+// -> 递归函数中耦合情况
+function factorial(num){
+    if (num==1) {
+        return 1;
+    } else {
+        return num*factorial(num-1);
+    }
 }
+out(factorial(5));
