91c0311298f1b336f1e301ed938c3b81fadf6d9a
Littlelumos_portable <littlelumos@foxmail.com>
2022-09-07 12:01:38 +0800

    update:简单原型语法

--- js/debugger.js
+++ js/debugger.js
@@ -10,11 +10,15 @@ function Person(name, age){
     this.age = age;
 }
 
-// -> forin对象
-var p1 = new Person("卡比同学", 10);
-for (const key in p1) {
-    if (Object.hasOwnProperty.call(p1, key)) {
-        const element = p1[key];
-        out(element);
+// -> 简单原型语法
+function Man(){
+}
+Man.prototype = {
+    name: "卡比同学",
+    age: 20,
+    sayName: function(){
+        out(this.name);
     }
 }
+var m = new Man();
+m.sayName()
