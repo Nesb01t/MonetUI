da5c0f2bf2ba199b3fd96ca66b56043320409f6e
Nesb01t <littlelumos@foxmail.com>
2022-09-18 20:09:57 +0800

    update: onUpdated

--- monetblog-vue/src/views/AboutView.vue
+++ monetblog-vue/src/views/AboutView.vue
@@ -1,11 +1,11 @@
 <template>
   <h1>vue3的生命周期</h1>
-  <div id="dom">{{msg}}</div>
+  <div id="dom">{{ msg }}</div>
 </template>
 
 
 <script>
-  import { reactive, toRefs, onBeforeMount, onMounted } from "vue"
+  import { reactive, toRefs, onBeforeMount, onMounted, onUpdated, onBeforeUpdate } from "vue"
   export default {
     name: "about",
     setup() {
@@ -15,10 +15,21 @@
       
       onBeforeMount(()=>{ // 数据渲染之前
         console.log("onBeforeMount", document.querySelector("#dom"))
+        setTimeout(()=>{
+          data.msg = "hello!!"
+        }, 2000)
       })
 
       onMounted(()=>{ // 数据渲染之后
         console.log("onBeforeMount", document.querySelector("#dom"))
+      })
+
+      onBeforeUpdate(()=>{ // 页面更新之前
+        console.log("onBeforeUpdate")
+      })
+
+      onUpdated(()=>{ // 页面更新之后-> 页面更改=Update
+        console.log("onUpdate")
       })
 
       return {
