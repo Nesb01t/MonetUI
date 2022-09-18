ed6ef6075d313f89ff33e8d6693bbbb7f17dd2d0
Nesb01t <littlelumos@foxmail.com>
2022-09-18 19:33:30 +0800

    update: toRefs和return data

--- monetblog-vue/src/views/HomeView.vue
+++ monetblog-vue/src/views/HomeView.vue
@@ -1,7 +1,19 @@
 <template>
-
+  <h1>hello world</h1>
+  <p v-text="name"></p>
+  <p>{{ name }}</p>
 </template>
 
 <script>
-  
+  import { reactive, toRefs } from "vue" // 引用什么就import
+  export default {
+    name: "home", //这个组件 HomeView.vue 的名字
+    setup() { // beforeCreated和created 生命周期
+      const data = reactive({
+        name: "小美",
+        age: 20
+      })
+      return { ...toRefs(data) }
+    }
+  }
 </script>
