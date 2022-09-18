e487149aa4488bc6ab9103c353af4cd53d0d7a49
Nesb01t <littlelumos@foxmail.com>
2022-09-18 22:37:35 +0800

    update: 双向绑定v-model

--- monetblog-vue/src/views/AboutView.vue
+++ monetblog-vue/src/views/AboutView.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1>v-on事件</h1>
-  <button v-on:click="handleClick">click me</button>
+  <h1>input 事件</h1>
+  <!-- v-model 双向绑定 -->
+  <input type="text" placeholder="请输入你的姓名" v-model="userName">
+  <textarea placeholder="请输入你的建议" cols="30" rows="10" v-model="userInput"></textarea>
+  <p>{{ userName }}</p>
+  <button>提交</button>
 </template>
 
 
@@ -10,7 +14,9 @@
     name: "about",
     setup() {
       const data = reactive({
-        msg: "message"
+        msg: "message",
+        userName: "",
+        userInput: ""
       })
       
       const handleClick = () => {
