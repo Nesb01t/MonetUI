9b7b2b0a60a6ba999b4408cbb390648edda792b9
Nesb01t <littlelumos@foxmail.com>
2022-09-18 20:02:57 +0800

    update: 渲染Mount之前和之后

--- monetblog-vue/src/views/AboutView.vue
+++ monetblog-vue/src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>vue3的生命周期</h1>
+  <div id="dom">{{msg}}</div>
 </template>
+
+
+<script>
+  import { reactive, toRefs, onBeforeMount, onMounted } from "vue"
+  export default {
+    name: "about",
+    setup() {
+      const data = reactive({
+        msg: "message"
+      })
+      
+      onBeforeMount(()=>{ // 数据渲染之前
+        console.log("onBeforeMount", document.querySelector("#dom"))
+      })
+
+      onMounted(()=>{ // 数据渲染之后
+        console.log("onBeforeMount", document.querySelector("#dom"))
+      })
+
+      return {
+        ...toRefs(data)
+      }
+    }
+  }
+</script>
