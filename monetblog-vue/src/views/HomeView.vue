757e54d29531574a47e76408d226cb56d3779d91
Nesb01t <littlelumos@foxmail.com>
2022-09-18 19:36:20 +0800

    update: v-html标签

--- monetblog-vue/src/views/HomeView.vue
+++ monetblog-vue/src/views/HomeView.vue
@@ -1,17 +1,18 @@
 <template>
-  <h1>hello world</h1>
-  <p v-text="name"></p>
-  <p>{{ name }}</p>
+  <!-- v-html -->
+  <p>{{ info }}</p>
+  <p v-html="info"></p>
 </template>
 
 <script>
   import { reactive, toRefs } from "vue" // 引用什么就import
-  export default {
+  export default { // 程序的js入口函数
     name: "home", //这个组件 HomeView.vue 的名字
     setup() { // beforeCreated和created 生命周期
       const data = reactive({
         name: "小美",
-        age: 20
+        age: 20,
+        info: "<i>富文本斜体字</i>"
       })
       return { ...toRefs(data) }
     }
