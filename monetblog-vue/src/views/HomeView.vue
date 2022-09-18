4e298f1bd2b2dc13bf1a0f1f429d1af491f05687
Nesb01t <littlelumos@foxmail.com>
2022-09-18 19:53:18 +0800

    update: ul->li + v-for遍历数组

--- monetblog-vue/src/views/HomeView.vue
+++ monetblog-vue/src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <p v-if="isRed">if</p>
-    <p v-else>else</p>
-  </div>
+  <ul>
+    <!-- 循环UserList -->
+    <!-- v-for=(每一个对象, 下标) in 数组 -->
+    <li v-for="(item, index) in userList" :key="index">
+      学生姓名: {{ item.userName }}
+      学生年龄: {{ item.userAge }}
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -11,10 +15,17 @@
     name: "home", //这个组件 HomeView.vue 的名字
     setup() { // beforeCreated和created 生命周期
       const data = reactive({
-        name: "小美",
-        age: 20,
-        info: "<i>富文本斜体字</i>",
-        isRed: true
+        userList: [
+          {
+            userName: "a", userAge: 10
+          },
+          {
+            userName: "b", userAge: 10
+          },
+          {
+            userName: "c", userAge: 10
+          }
+        ]
       })
       return { ...toRefs(data) }
     }
