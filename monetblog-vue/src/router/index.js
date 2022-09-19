853a3773cb99e837e70c3278a1a9b95c0f73ca16
Nesb01t <littlelumos@foxmail.com>
2022-09-19 10:14:23 +0800

    update: 优化Router

--- monetblog-vue/src/router/index.js
+++ monetblog-vue/src/router/index.js
@@ -1,24 +1,27 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+// WebHistory历史模式, WebHashHistory哈希历史模式
+import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/maps',
+    name: 'maps',
+    component: () => import("../views/Maps.vue")
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
