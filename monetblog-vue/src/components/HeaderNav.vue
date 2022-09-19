b946fab62aae01fe173370ee8aa71ccc5cb6fe5f
Nesb01t <littlelumos@foxmail.com>
2022-09-19 22:54:11 +0800

    update: 分支提交

--- monetblog-vue/src/components/HeaderNav.vue
+++ monetblog-vue/src/components/HeaderNav.vue
@@ -3,21 +3,24 @@
     class="headerNav"
     mode="horizontal"
     @select="handleSelect"
+    router
   >
-    <el-menu-item index="1">Home</el-menu-item>
-    <el-menu-item index="2" v-link="{ path: '/terraria'}">Terraria</el-menu-item>
-    <el-menu-item index="3">WowMaps</el-menu-item>
-    <el-menu-item index="4" disabled>More</el-menu-item>
+    <el-menu-item index="/">Home</el-menu-item>
+    <el-menu-item index="/terraria">Terraria</el-menu-item>
+    <el-menu-item index="/wowmaps">WowMaps</el-menu-item>
+    <el-menu-item disabled>More</el-menu-item>
   </el-menu>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
 const handleSelect = (key, keyPath) => {
-  console.log(key, keyPath)
+  console.log(key)
 }
+
 </script>
 
 <style>
-
 </style>
--- monetblog-vue/src/views/Home.vue
+++ monetblog-vue/src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  
+  <div id="home">
+    <h1>Readme</h1><br>
+    <p>待补充...</p>
+  </div>
 </template>
 
 <script>
 export default {
-
+    
 }
 </script>
 
 <style>
-
+.home {
+    font-size: 30px;
+}
 </style>
