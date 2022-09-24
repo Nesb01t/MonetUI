a08f37fbc6aab0fa94c1d3cb4498af728aaf0d78
Nesb01t <littlelumos@foxmail.com>
2022-09-24 21:42:54 +0800

    update: topnav

--- monetblog-vue/src/components/HeaderNav.vue
+++ monetblog-vue/src/components/HeaderNav.vue
@@ -1,33 +1,41 @@
 <template>
-  <el-menu
-    class="headerNav"
-    mode="horizontal"
-    @select="handleSelect"
-    router
-  >
-    <el-button class="logo" type="primary" bg>MonetBlog</el-button>
-    <el-menu-item index="/">Home</el-menu-item>
-    <el-menu-item index="/terraria">Terraria</el-menu-item>
-    <el-menu-item index="/wowmaps">WowMaps</el-menu-item>
-    <el-menu-item disabled>More</el-menu-item>
-  </el-menu>
+  <div class="nav">
+    <div class="logo">
+      <el-button type="primary" bg>MonetBlog</el-button>
+    </div>
+    
+    <div class="menu">
+      <el-menu
+        mode="horizontal"
+        @select="handleSelect"
+        router
+      >
+        <el-menu-item index="/">Home</el-menu-item>
+        <el-menu-item index="/terraria">Terraria</el-menu-item>
+        <el-menu-item index="/wowmaps">WowMaps</el-menu-item>
+        <el-menu-item disabled>More</el-menu-item>
+      </el-menu>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-const activeIndex = ref('1')
-const handleSelect = (key, keyPath) => {
-  console.log(key)
-}
 
 </script>
 
 <style>
+.nav {
+  display: flex;
+  flex-flow: 1;
+}
+
 .logo {
-  margin-left: 2%;
-  margin-right: 2%;
-  margin-top: auto;
-  margin-bottom: auto;
+  width: 20%;
+  
+  display: flex;
+  justify-content: center;
+}
+.menu {
+  width: 80%;
 }
 </style>
